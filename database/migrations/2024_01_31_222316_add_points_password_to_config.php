feb208cfdcb95590f0d023f0e93547b951a66aac
haytham <haytham>
2024-02-01 05:13:34 +0300

    all good work togther

--- database/migrations/2024_01_31_222316_add_points_password_to_config.php
+++ database/migrations/2024_01_31_222316_add_points_password_to_config.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\ConfigOption;
+use Illuminate\Support\Facades\Hash;
 class AddPointsPasswordToConfig extends Migration
 {
     /**
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -10,7 +10,7 @@
                     <button type="button" class="icon-button displayAnotherModel" data-bs-toggle="modal"
                         data-bs-target="#notificationModal">
                         <span class="material-icons">notifications</span>
-                        <span class="icon-button__badge">0</span>
+                        <span class="icon-button__badge" id="countNoti">0</span>
                     </button>
                     @endauth
 
@@ -30,39 +30,19 @@
                         @endguest
 
 
-                        <div class="modal fade" id="notificationModal" tabindex="-1"
+                        <div class="modal fade noti" id="notificationModal" tabindex="-1"
                             aria-labelledby="notificationModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <h1 class="modal-title fs-5" id="notificationModalLabel">Notification Modal</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        <button type="button" class="btn-close" id="closeNoti" data-bs-dismiss="modal"
                                             aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
                                         You Don't have any notifaction
                                     </div>
                                     <div class="modal-footer">
-                                        @auth
-                                        <?php
-                                            $user = auth()->user();
-
-                                            if ($user && $userData = $user->userData) {
-                                                $points = $userData->points;
-                                            } else {
-                                                $points = null;
-                                            }
-                                            ?>
-                                        <div class="user-points">
-                                            @if($points !== null)
-                                            <p class="points-label">Total Points: <span
-                                                    class="points-display">{{ $points }}</span></p>
-                                            @else
-                                            <p class="error-message">User not authenticated or user data not found</p>
-                                            @endif
-                                        </div>
-                                        @endauth
-
 
                                     </div>
                                 </div>
